6ca7cf391b408384260804261c99ab499b4c9e74
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-08-12 00:01:39 +0600

    Routing

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Auth;
 
 
 
-Route::get('/', 'PagesController@index')->name('home');
-Route::get('/admin/dashboard', 'PagesController@dashboard')->name('admin.dashboard');
+Route::get('/','PagesController@index')->name('home');
+Route::get('/admin/dashboard','PagesController@dashboard')->name('admin.dashboard');
 
 
 
 
 
-Auth::routes();
+//Auth::routes();
 
-Route::get ('/home', 'HomeController@index')->name('home');
+//Route::get ('/home', 'HomeController@index')->name('home');
 
 
